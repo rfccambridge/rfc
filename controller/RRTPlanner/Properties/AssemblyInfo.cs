d933ab2fec59153163194fa43542fb0c3eab4577
kmod <kmod@fas.harvard.edu>
2007-10-03 15:27:20 +0000

    moving some projects around (changing directory structure)

--- controller/RRTPlanner/Properties/AssemblyInfo.cs
+++ controller/RRTPlanner/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SoccerSim")]
+[assembly: AssemblyTitle("RRTPlanner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SoccerSim")]
+[assembly: AssemblyProduct("RRTPlanner")]
 [assembly: AssemblyCopyright("Copyright ©  2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1c6d6f5b-a6bd-4c08-944d-cb7c6a3e6076")]
+[assembly: Guid("ad11c67b-cc08-4095-810d-f3ae2d134a7a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
