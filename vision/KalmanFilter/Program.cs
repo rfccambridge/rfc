e820098a30a62f64042fb4301bd660b5aa62263e
Alexei Colin <alexei.co@gmail.com>
2007-10-20 21:14:16 +0000

    Forwarded debug-output to a file

--- vision/KalmanFilter/Program.cs
+++ vision/KalmanFilter/Program.cs
@@ -1,70 +1,64 @@
 using System;
 using System.Collections;
 using System.Windows.Forms;
+using System.IO;
 
 namespace KalmanFilter
 {
     static class Program
     {
+        // All output goes to this file
+        const string OUTPUT_FILENAME = "kalman_out.txt";
+
+       
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void pAL(ArrayList a)
-        {
-            for (int i = 0; i < a.Count; i++)
-            {
-                //Console.Write(a[i]);
-                Console.Write("{0:G4}, ",a[i]);
-            }
-            Console.Write("\n");
-        }
-
-        static double erfi(double z)
-        {
-            return ( Math.Sqrt(Math.PI)/2.0 )* (z + Math.PI * Math.Pow(z,3.0)/12.0 + 7.0*Math.Pow(Math.PI,2.0)*Math.Pow(z,5.0)/480.0 + 127.0* Math.Pow(Math.PI, 3.0) *Math.Pow(z, 7.0)/ 40320.0 + 4369.0* Math.Pow(Math.PI, 4.0) *Math.Pow(z, 9.0)/ 5806080.0);
-        }
-
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            Console.WriteLine("George: hello robin");
+
+            // All output goes to file OUTPUT_FILENAME
+            TextWriter twOut = new StreamWriter(OUTPUT_FILENAME);
+
             filter f = new filter();
             f.initialize(0, 0, 0, 0, 0, 100);
-            Console.WriteLine("Initial State");
-            pAL(f.get_state(0, 0));
-            Console.WriteLine("Initial P has doubt = 100.0");
 
-            Random n = new Random();
+            twOut.WriteLine("Initial State: " + ALToString(f.get_state(0, 0)));
+            twOut.WriteLine("Initial P has doubt = 100.0");
+            twOut.WriteLine();
+
+            Random n = new Random();           
                                     
             for (double i = 1.0; i < 300.0; i = i + 1.0)
             {
                 double e1 = n.NextDouble();
                 double e2 = n.NextDouble();
                 f.update(0, 1, i + erfi(2.0 * e1 - 1.0) * 0.2, i + erfi(2.0 * e2 - 1.0) * 0.2);
-                Console.Write(i);
-                Console.Write(", ");
-                Console.Write("{0:G4}",i + erfi(2.0 * e1 - 1.0) * 0.2);
-                Console.Write(", ");
-                Console.Write("{0:G4}", i + erfi(2.0 * e2 - 1.0) * 0.2);
-                Console.Write(", ");
-                pAL(f.get_state(0, 0));
+
+                twOut.Write(i + ":\t" +
+                            String.Format("{0:G4}", i + erfi(2.0 * e1 - 1.0) * 0.2) + "\t" +
+                            String.Format("{0:G4}", i + erfi(2.0 * e2 - 1.0) * 0.2) + "\t\t" +
+                            "STATE: " + ALToString(f.get_state(0,0)));
+                
             }
+
+            twOut.WriteLine();
+
             for (double i = 1.0; i < 200.0; i = i + 1.0)
             {
                 double e1 = n.NextDouble();
                 double e2 = n.NextDouble();
                 f.update(0, 1, i + erfi(2.0 * e1 - 1.0) * 0.2, i + erfi(2.0 * e2 - 1.0) * 0.2);
-                Console.Write(i);
-                Console.Write(", ");
-                Console.Write("{0:G4}", i + erfi(2.0 * e1 - 1.0) * 0.2);
-                Console.Write(", ");
-                Console.Write("{0:G4}", i + erfi(2.0 * e2 - 1.0) * 0.2);
-                Console.Write(", ");
-                pAL(f.get_state(0, 0));
+
+                twOut.Write(i + ":\t" +
+                            String.Format("{0:G4}", i + erfi(2.0 * e1 - 1.0) * 0.2) + "\t" +
+                            String.Format("{0:G4}", i + erfi(2.0 * e2 - 1.0) * 0.2) + "\t\t" +
+                            "STATE: " + ALToString(f.get_state(0, 0)));
             }
+
+            twOut.Close();
+
             //george.display();
             /*george.update(0, 1, 2, 2);
             ArrayList george_state = george.get_state(0, 1);
@@ -148,5 +142,34 @@ namespace KalmanFilter
 
             product.display();*/
         }
+
+        static string ALToString(ArrayList arList) {
+            string sOut = "";
+
+            for (int i = 0; i < arList.Count; i++)
+                sOut += String.Format("{0:G4}\t", arList[i]);
+            sOut += Environment.NewLine;
+
+            return sOut;
+        }
+
+        static void printAL(ArrayList a) {
+
+            for (int i = 0; i < a.Count; i++) {
+                Console.Write("{0:G4}, ", a[i]);
+            }
+            Console.WriteLine();
+
+        }
+        
+        static double erfi(double z) {
+            return (Math.Sqrt(Math.PI) / 2.0) * (z + Math.PI * Math.Pow(z, 3.0) / 12.0 + 
+                7.0 * Math.Pow(Math.PI, 2.0) * Math.Pow(z, 5.0) / 480.0 + 
+                127.0 * Math.Pow(Math.PI, 3.0) * Math.Pow(z, 7.0) / 40320.0 + 
+                4369.0 * Math.Pow(Math.PI, 4.0) * Math.Pow(z, 9.0) / 5806080.0);
+        }
     }
+
 }
+
+
