0f6d25869701ecc0db0eab6e809e46bacaccc5d4
kmod <kmod@fas.harvard.edu>
2007-11-06 04:20:39 +0000

    forgot to svn add these (a while ago...)

---
+++ control/CoreRobotics/MulticastRefBoxListener.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using Robocup.Core;
+
+namespace Robocup.CoreRobotics
+{
+    public class MulticastRefBoxListener : IRefBoxListener
+    {
+        public const char HALT = 'H';
+        public const char STOP = 'S';
+        public const char READY = ' ';
+        public const char START = 's';
+        public const char KICKOFF_YELLOW = 'k';
+        public const char PENALTY_YELLOW = 'p';
+        public const char DIRECT_YELLOW = 'f';
+        public const char INDIRECT_YELLOW = 'i';
+        public const char TIMEOUT_YELLOW = 't';
+        public const char TIMEOUT_END_YELLOW = 'z';
+        
+        public const char KICKOFF_BLUE = 'K';
+        public const char PENALTY_BLUE = 'P';
+        public const char DIRECT_BLUE = 'F';
+        public const char INDIRECT_BLUE = 'I';
+        public const char TIMEOUT_BLUE = 'T';
+        public const char TIMEOUT_END_BLUE = 'Z';
+        
+        public const char CANCEL = 'c';
+
+        public struct RefboxPacket
+        {
+            public char cmd;
+            public byte cmd_counter;    // counter for current command
+            public byte goals_blue;      // current score for blue team
+            public byte goals_yellow;    // current score for yellow team
+            public short time_remaining; // seconds remaining for current game stage (network byte order)
+            
+            public byte[] toByte()
+            {
+                int len = Marshal.SizeOf(this);
+                byte[] arr = new byte[len];
+                IntPtr ptr = Marshal.AllocHGlobal(len);
+                Marshal.StructureToPtr(this, ptr, true);
+                Marshal.Copy(ptr, arr, 0, len);
+                Marshal.FreeHGlobal(ptr);
+                return arr;
+
+            }
+
+            public void setVals(byte[] dataBytes)
+            {
+                RefboxPacket obj = new RefboxPacket();
+                int len = Marshal.SizeOf(obj);
+                IntPtr i = Marshal.AllocHGlobal(len);
+                Marshal.Copy(dataBytes, 0, i, len);
+                obj = (RefboxPacket)Marshal.PtrToStructure(i, obj.GetType());
+                Marshal.FreeHGlobal(i);
+                this = obj;
+            }
+
+            public int getSize()
+            {
+                return Marshal.SizeOf(this);
+            }
+        };
+
+
+        class StateObject
+        {
+            public Socket sock;
+            public byte[] buffer;
+            public RefboxPacket packet;
+            public EndPoint ep;
+
+            public StateObject()
+            {
+                packet = new RefboxPacket();
+                buffer = new byte[packet.getSize()];
+            }
+        }
+
+
+        volatile bool running;
+        Socket s;
+        EndPoint ep;
+        StateObject so;
+        RefboxPacket packet;
+
+        public MulticastRefBoxListener(int port)
+        {
+            packet = new RefboxPacket();
+            packet.cmd = 'H';
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            ep = (EndPoint)new IPEndPoint(IPAddress.Any, port);
+            s.Bind(ep);
+            s.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(IPAddress.Parse("224.5.23.1")));
+
+            so = new StateObject();
+            so.sock = s;
+            so.ep = ep;
+            running = false;
+
+            start();
+        }
+
+        void ReceiveRefboxPacket(IAsyncResult result)
+        {
+            StateObject so = (StateObject)result.AsyncState;
+            packet = new RefboxPacket();
+            if (so.sock.EndReceive(result) == packet.getSize())
+            {
+                
+
+                packet.setVals(so.buffer);
+                /*Console.WriteLine("command: " + packet.cmd + " counter: " + packet.cmd_counter
+                    + " blue: " + packet.goals_blue + " yellow: " + packet.goals_yellow+
+                    " time left: " + packet.time_remaining);*/
+                
+            }
+
+            if (running)
+            {
+                so.sock.BeginReceiveFrom(so.buffer, 0, so.packet.getSize(), SocketFlags.None, ref so.ep, new AsyncCallback(ReceiveRefboxPacket), so);
+            }
+        }
+
+
+        public void start()
+        {
+            running = true;
+            s.BeginReceiveFrom(so.buffer, 0, so.packet.getSize(), SocketFlags.None, ref ep, new AsyncCallback(ReceiveRefboxPacket), so);
+        }
+
+        public void stop()
+        {
+            running = false;
+        }
+
+        public int getCmdCounter()
+        {
+            return packet.cmd_counter;
+        }
+
+        public char getLastCommand()
+        {
+            return packet.cmd;
+        }
+
+    }
+}
---
+++ control/MotionPlanning/NavigationPlanner.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Robocup.Core;
+using Robocup.CoreRobotics;
+using Navigation;
+
+namespace Robocup.MotionControl
+{
+    /// <summary>
+    /// Takes an INavigator and an IMovement and puts them together to create an IMotionPlanner
+    /// </summary>
+    abstract public class NavigationPlanner : IMotionPlanner
+    {
+        private INavigator navigator;
+        private IMovement movement;
+        public NavigationPlanner(INavigator navigator, IMovement movement)
+        {
+            this.navigator = navigator;
+            this.movement = movement;
+        }
+
+        public MotionPlanningResults PlanMotion(int id, RobotInfo desiredState, IPredictor predictor, double avoidBallRadius)
+        {
+            NavigationResults results = navigator.navigate(id, predictor.getCurrentInformation(id).Position,
+                desiredState.Position, predictor.getOurTeamInfo().ToArray(), predictor.getTheirTeamInfo().ToArray(),
+                predictor.getBallInfo(), avoidBallRadius);
+            WheelSpeeds speeds = movement.calculateWheelSpeeds(predictor, id, predictor.getCurrentInformation(id), results);
+            return new MotionPlanningResults(speeds);
+        }
+
+        public void DrawLast(System.Drawing.Graphics g, ICoordinateConverter c)
+        {
+            navigator.drawLast(g, c);
+        }
+    }
+    public class CurrentPlanner : NavigationPlanner
+    {
+        public CurrentPlanner()
+            :
+            base(new Navigation.Current.CurrentNavigator(), new FourWheeledMovement()) { }
+    }
+}
