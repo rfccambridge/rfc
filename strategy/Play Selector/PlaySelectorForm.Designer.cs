1ca7376361c274909852480f46d45f4540aff038
Alexei Colin <alexei.co@gmail.com>
2009-04-07 23:28:16 +0000

    PlaySelector Form -- some more run away files from previous commit. On behalf of Richard.

---
+++ strategy/Play Selector/PlaySelectorForm.Designer.cs
@@ -0,0 +1,58 @@
+namespace Robocup.Plays
+{
+    partial class PlaySelectorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(291, 289);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // PlaySelectorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 306);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "PlaySelectorForm";
+            this.Text = "PlaySelectorForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
---
+++ strategy/Play Selector/PlaySelectorForm.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Robocup.Plays
+{
+    public partial class PlaySelectorForm : Form
+    {
+        public PlaySelectorForm(List<InterpreterPlay> plays)
+        {
+            InitializeComponent();
+            this.checkedListBox1.Items.Clear();
+            foreach (InterpreterPlay play in plays)
+                this.checkedListBox1.Items.Add(play);
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+        }
+
+        // Activates the move button if there are checked items.
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            ((InterpreterPlay)(this.checkedListBox1.Items[e.Index])).isEnabled = (e.NewValue == CheckState.Checked);
+        }
+    }
+
+}
