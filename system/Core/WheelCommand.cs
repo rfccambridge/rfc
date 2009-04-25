7200a76ab4514dd4036e17067d5d662448e6a8b4
Alexei Colin <alexei.co@gmail.com>
2009-04-25 07:32:42 +0000

    Part of previous commit: dissolved the MessageTypes project and moved VisionMessage.cs and WheelCommand.cs to Core, to avoid circular dependencies.

---
+++ system/Core/WheelCommand.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robocup.Core
+{
+    [Serializable]
+    public class RobotCommand {
+        public enum Command
+        {
+            MOVE,
+            KICK,
+            BEAMKICK,
+            CHARGE
+        };
+        public WheelSpeeds speeds;
+        public int ID;
+        public Command command;
+        
+        public RobotCommand(int ID, WheelSpeeds speeds) {
+            init(ID, Command.MOVE, speeds);
+        }
+        public RobotCommand(int ID, Command command, WheelSpeeds speeds)
+        {
+            init(ID, command, speeds);
+        }
+        private void init(int ID, Command command, WheelSpeeds speeds)
+        {
+            this.speeds = speeds;
+            this.ID = ID;
+            this.command = command;
+        }
+    }
+}
