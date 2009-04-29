4ee2d97c4c133435e8bb4e310d25dc055c7dbe22
Daniel Robinson <robins@fas.harvard.edu>
2009-04-29 04:24:51 +0000

    Added two additional constructors for ActionDefinition. It is now possible to construct an action with a String representing a play to call (rather than a function delegate representing robot actions). This occurs in the action assignPlay. Such ActionDefinitions are used in PlaySelector to select plays based on the main play.
    Also added a constructor that takes no arguments, which allows the play function nullAction to create an ActionDefinition that the PlaySelector ignores.

--- strategy/Core Play Files/ActionDefinition.cs
+++ strategy/Core Play Files/ActionDefinition.cs
@@ -9,6 +9,11 @@ namespace Robocup.Plays
     public class ActionDefinition
     {
         private Action action;
+
+        // NEW: Allow actions to include a play assignment
+        public String AssignPlay;
+        public bool isEmpty = false;
+        
         public Action runAction
         {
             get { return action; }
@@ -18,11 +23,34 @@ namespace Robocup.Plays
         {
             get { return robotsinvolved; }
         }
+
+        /// <summary>
+        /// Initialized with an Action and with the ids of the robots that are involved
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="robotsInvolved"></param>
         public ActionDefinition(Action action, params int[] robotsInvolved)
         {
             this.action = action;
             this.robotsinvolved = robotsInvolved;
         }
+
+        /// <summary>
+        /// An ActionDefinition with just a play to assign
+        /// </summary>
+        /// <param name="assignPlay"></param>
+        public ActionDefinition(String assignPlay) {
+            AssignPlay = assignPlay;
+        }
+
+        /// <summary>
+        /// A null action
+        /// </summary>
+        /// <returns></returns>
+        public ActionDefinition() {
+            isEmpty = true;
+        }
+
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder("robots: ");
