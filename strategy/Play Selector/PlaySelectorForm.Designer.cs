30b1e10a4013729444ef721e8faded8ebe8ade2a
jgong <ygong@fas.harvard.edu>
2009-05-05 20:10:51 +0000

    select/select none on PlaySelectorForm

--- strategy/Play Selector/PlaySelectorForm.Designer.cs
+++ strategy/Play Selector/PlaySelectorForm.Designer.cs
@@ -29,6 +29,8 @@ namespace Robocup.Plays
         private void InitializeComponent()
         {
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkedListBox1
@@ -40,21 +42,50 @@ namespace Robocup.Plays
             this.checkedListBox1.TabIndex = 0;
             this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select All";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.click_Button1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select None";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.click_Button2);
+            // 
             // PlaySelectorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(303, 306);
+            this.ClientSize = new System.Drawing.Size(296, 339);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "PlaySelectorForm";
             this.Text = "PlaySelectorForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PlaySelectorForm_FormClosing);
             this.ResumeLayout(false);
+            this.Resize += new System.EventHandler(PlaySelectorForm_Resize);
 
+        }
+
+        void PlaySelectorForm_Resize(object sender, System.EventArgs e) {
+            this.checkedListBox1.Width = this.Width - 10;
         }
 
         #endregion
 
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- strategy/Play Selector/PlaySelectorForm.cs
+++ strategy/Play Selector/PlaySelectorForm.cs
@@ -27,12 +27,26 @@ namespace Robocup.Plays
         // Activates the move button if there are checked items.
         private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
         {
-            ((InterpreterPlay)(this.checkedListBox1.Items[e.Index])).isEnabled = (e.NewValue == CheckState.Checked);
+            InterpreterPlay play = (InterpreterPlay)(this.checkedListBox1.Items[e.Index]);
+            play.isEnabled = (e.NewValue == CheckState.Checked);
+            Console.WriteLine("Play is " + (play.isEnabled ? "ENABLED " : "DISABLED: ") + play.Name);
         }
 
         private void PlaySelectorForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             e.Cancel = true;
+        }
+
+        private void click_Button1(object sender, System.EventArgs e) {
+            for (int i = 0; i < this.checkedListBox1.Items.Count; ++i)
+                this.checkedListBox1.SetItemChecked(i, true);
+        }
+
+
+        private void click_Button2(object sender, System.EventArgs e) {
+            for (int i = 0; i < this.checkedListBox1.Items.Count; ++i)
+                this.checkedListBox1.SetItemChecked(i, false);
+
         }
     }
 
