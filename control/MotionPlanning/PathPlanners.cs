d7d3818578b78db8a68bff8f959170ef2c197d0c
Svilen Kanev <skanev>
2011-02-19 09:03:54 -0500

    NumberRange.closest to center was used wrongly. hysteresis value limited the max angle you could go to. Testing different ways of using hysteresis.

--- control/MotionPlanning/PathPlanners.cs
+++ control/MotionPlanning/PathPlanners.cs
@@ -16,23 +16,27 @@ namespace Robocup.MotionControl
 {
     public class DumbPathPlanner : IPathPlanner
     {
-        public DumbPathPlanner() {
+        public DumbPathPlanner()
+        {
 
         }
-        
-        public RobotPath GetPath(Team team, int id, RobotInfo desiredState, IPredictor predictor, double avoidBallRadius) {
+
+        public RobotPath GetPath(Team team, int id, RobotInfo desiredState, IPredictor predictor, double avoidBallRadius)
+        {
             List<RobotInfo> waypoints = new List<RobotInfo>();
             waypoints.Add(desiredState);
             return new RobotPath(waypoints);
         }
-      
-        public void ReloadConstants() { 
-        
+
+        public void ReloadConstants()
+        {
+
         }
     }
 
     // PathPlanner version of the BugNavigator INavigator
-    public class BugNavigatorPlanner : NavigatorPlanner {
+    public class BugNavigatorPlanner : NavigatorPlanner
+    {
         static BugNavigator navigator = new BugNavigator();
 
         public BugNavigatorPlanner() : base(navigator) { }
@@ -131,11 +135,11 @@ namespace Robocup.MotionControl
 
     public class TangentBugPlanner : IPathPlanner
     {
+        int NUM_ROBOTS;
         double LOOK_AHEAD_DIST;
         double AVOID_DIST;
         double MIN_ABS_VAL_STICK;
         public double WAYPOINT_DIST;
-        double EXTRA_GOAL_DIST;
         double BOUNDARY_AVOID;
 
         double MIN_X_ROBOT_BOUNDARY;
@@ -143,19 +147,23 @@ namespace Robocup.MotionControl
         double MIN_Y_ROBOT_BOUNDARY;
         double MAX_Y_ROBOT_BOUNDARY;
 
-    	double STEADY_STATE_SPEED;
+        double STEADY_STATE_SPEED;
 
         // line segments that must be avoided- field edges and goals
         List<Line> boundary_lines;
 
-        double previousAngle;
+        double[] previousAngles;
 
-        Vector2 lastWaypoint;        
+        Vector2 lastWaypoint;
 
         public TangentBugPlanner()
-        {            
+        {
             ReloadConstants();
-            previousAngle = 0;
+
+            previousAngles = new double[NUM_ROBOTS];
+
+            for (int i = 0; i < NUM_ROBOTS; i++)
+                previousAngles[i] = 0;
         }
 
         public RobotPath GetPath(Team team, int id, RobotInfo desiredState, IPredictor predictor, double avoidBallRadius)
@@ -180,9 +188,10 @@ namespace Robocup.MotionControl
             Vector2 directionToGoal = (end - start);
 
             // if close to the goal, decrease look ahead distance
-            if (directionToGoal.magnitudeSq() < WAYPOINT_DIST * WAYPOINT_DIST) {
+            if (directionToGoal.magnitudeSq() < WAYPOINT_DIST * WAYPOINT_DIST)
+            {
                 //Note: this implies zero velocity!
-				return new RobotPath(team, id, desiredState);
+                return new RobotPath(team, id, desiredState);
             }
 
             // get desired waypoint
@@ -190,16 +199,19 @@ namespace Robocup.MotionControl
             List<double> obstacleSizes = new List<double>();
             foreach (RobotInfo info in predictor.GetRobots())
             {
-                if (info.ID != id) {
+                if (info.ID != id)
+                {
                     obstaclePositions.Add(info.Position);
                     obstacleSizes.Add(AVOID_DIST);
                 }
                 //Console.WriteLine(info.ToString());
             }
 
-            if (avoidBallRadius > 0){
+            if (avoidBallRadius > 0)
+            {
                 BallInfo ballInfo = predictor.GetBall();
-                if (ballInfo != null) {
+                if (ballInfo != null)
+                {
                     obstaclePositions.Add(ballInfo.Position);
                     obstacleSizes.Add(avoidBallRadius);
                 }
@@ -221,7 +233,8 @@ namespace Robocup.MotionControl
             double angleAvoid;
 
             // remove impossible angles
-            for (int i=0; i < obstaclePositions.Count; i++) {
+            for (int i = 0; i < obstaclePositions.Count; i++)
+            {
 
                 Vector2 obstPos = obstaclePositions[i];
                 // get distance to obstacle
@@ -235,7 +248,7 @@ namespace Robocup.MotionControl
                 }
 
                 // get angle to obstacle
-                obstacleAngle = (obstPos-start).cartesianAngle();
+                obstacleAngle = (obstPos - start).cartesianAngle();
                 angleDiff = UsefulFunctions.angleDifference(finalDirectionAngle, obstacleAngle);
 
                 // get amount around angle to avoid
@@ -247,20 +260,23 @@ namespace Robocup.MotionControl
                 //Console.WriteLine("Avoiding from " + (angleDiff - angleAvoid) + " to " + (angleDiff + angleAvoid));
 
                 // remove this range of the possible angles
-                angle_range.remove(angleDiff - angleAvoid, angleDiff + angleAvoid);
+                angle_range.Remove(angleDiff - angleAvoid, angleDiff + angleAvoid);
 
             }
-            
+
             // avoid boundary lines, but only if inside boundaries
             double dist_to_line;
             // do not perform this loop if not inside boundaries
             if (start.X > MIN_X_ROBOT_BOUNDARY && start.X < MAX_X_ROBOT_BOUNDARY &&
-                start.Y > MIN_Y_ROBOT_BOUNDARY && start.Y < MAX_Y_ROBOT_BOUNDARY) {
-                foreach (Line l in boundary_lines) {
+                start.Y > MIN_Y_ROBOT_BOUNDARY && start.Y < MAX_Y_ROBOT_BOUNDARY)
+            {
+                foreach (Line l in boundary_lines)
+                {
                     // get distance from line
                     dist_to_line = l.distFromLine(start);
                     // ignore lines more than a certain distance
-                    if (dist_to_line > BOUNDARY_AVOID) {
+                    if (dist_to_line > BOUNDARY_AVOID)
+                    {
                         continue;
                     }
 
@@ -273,34 +289,71 @@ namespace Robocup.MotionControl
                     angleDiff = UsefulFunctions.angleDifference(finalDirectionAngle, dir_to_point.cartesianAngle());
 
                     // remove this range of the possible angles
-                    angle_range.remove(angleDiff - angleAvoid, angleDiff + angleAvoid);
+                    angle_range.Remove(angleDiff - angleAvoid, angleDiff + angleAvoid);
                 }
             }
 
-            // use closest angle to center
             double bestAngle;
+            // find closest angle to center in both directions
+            double bestAngleCCW, bestAngleCW;
+            angle_range.ClosestToCenter(-Math.PI, Math.PI, out bestAngleCW, out bestAngleCCW);
+
+            // both directions are bad - no path
+            if ((bestAngleCW == -Math.PI) && (bestAngleCCW == Math.PI))
+            {
+                Console.WriteLine("NO GOOD ROUTE ({0})!!!", id);
+                previousAngles[id] = 0;
+                return new RobotPath(team, id);
+            }
+
+            if ((bestAngleCW > -Math.PI) && (bestAngleCCW == Math.PI))
+                bestAngle = bestAngleCW;
+            else if ((bestAngleCW == -Math.PI) && (bestAngleCCW < Math.PI))
+                bestAngle = bestAngleCCW;
+            else //hysteresis: chose the side last chosen for this robot
+            {
+                //bestAngle = Math.Min(bestAngleCCW, Math.PI + bestAngleCW);
+                Console.WriteLine("CCW: {0}, CW: {1}", bestAngleCCW, Math.PI + bestAngleCW);
+                //bestAngle = (previousAngles[id] > 0) ? bestAngleCCW : bestAngleCW;
+                Vector2 wptCW = start + finalDirection.rotate(bestAngleCW);
+                Vector2 wptCCW = start + finalDirection.rotate(bestAngleCCW);
+                double deltaDist = Math.Sqrt(wptCW.distanceSq(desiredState.Position)) -
+                    Math.Sqrt(wptCCW.distanceSq(desiredState.Position));
+                double deltaAngle = UsefulFunctions.angleDifference(currentState.Velocity.cartesianAngle(),
+                        directionToGoal.cartesianAngle());
+
+                if(Math.Abs(deltaAngle) < Math.PI / 60)
+                //if (Math.Abs(deltaDist) < 0.05) //Difference too small - hysteresis
+                    //bestAngle = (previousAngles[id] > 0) ? bestAngleCCW : bestAngleCW;
+                    bestAngle = UsefulFunctions.angleDifference(currentState.Velocity.cartesianAngle(),
+                        directionToGoal.cartesianAngle()) > 0 ? bestAngleCCW : bestAngleCW;
+                else
+                    //bestAngle = (deltaDist > 0) ? bestAngleCCW : bestAngleCW;
+                    bestAngle = (bestAngleCCW < Math.PI + bestAngleCW) ? bestAngleCCW : bestAngleCW;
+            }
 
             // Once a side has been picked, stick to it
             // THIS IS A LITTLE BIT OF A HACK!
-            if (previousAngle > MIN_ABS_VAL_STICK)
-            {
-                 bestAngle = angle_range.closestToCenter(-MIN_ABS_VAL_STICK, 100);
-            }
-            else if (previousAngle < -MIN_ABS_VAL_STICK)
-            {
-                bestAngle = angle_range.closestToCenter(-100, MIN_ABS_VAL_STICK);
-            }
-            else
-            {
-                bestAngle = angle_range.closestToCenter(-100, 100);
-            }
+            //if (previousAngles[id] > MIN_ABS_VAL_STICK)
+            //{
+            //    bestAngle = angle_range.ClosestToCenter(-MIN_ABS_VAL_STICK, 100);
+            //}
+            //else if (previousAngles[id] < -MIN_ABS_VAL_STICK)
+            //{
+            //    bestAngle = angle_range.ClosestToCenter(-100, MIN_ABS_VAL_STICK);
+            //}
+            //else
+            //{
+            //    bestAngle = angle_range.ClosestToCenter(-100, 100);
+            //}
 
-            if (bestAngle == -1000) {
-                // no good route- return empty path
-                Console.WriteLine("NO GOOD ROUTE!");
-                return new RobotPath(team, id);
+            //if (bestAngle == -1000)
+            //{
+            //    // no good route- return empty path
+            //    Console.WriteLine("NO GOOD ROUTE!");
+            //    return new RobotPath(team, id);
 
-            }
+            //}
 
             /*
             if (Math.Abs(bestAngle) > MIN_ABS_VAL_STICK && Math.Abs(previousAngle) > MIN_ABS_VAL_STICK && 
@@ -310,19 +363,19 @@ namespace Robocup.MotionControl
             }
              */
 
-            finalDirection = rotateDegree(finalDirection, bestAngle);
+            finalDirection = finalDirection.rotate(bestAngle);
 
-            previousAngle = bestAngle;
+            previousAngles[id] = bestAngle;
 
             lastWaypoint = start + finalDirection;
 
             List<RobotInfo> waypoints = new List<RobotInfo>();
 
-        	Vector2 velocity = finalDirection.normalizeToLength(STEADY_STATE_SPEED);
-			//State representing carrot on a stick
-            RobotInfo carrotState = new RobotInfo(lastWaypoint, 
-				velocity, 0, desiredState.Orientation, team, id);
-			waypoints.Add(carrotState);
+            Vector2 velocity = finalDirection.normalizeToLength(STEADY_STATE_SPEED);
+            //State representing carrot on a stick
+            RobotInfo carrotState = new RobotInfo(lastWaypoint,
+                velocity, 0, desiredState.Orientation, team, id);
+            waypoints.Add(carrotState);
 
 
             //Add more waypoints along the line carrotState -> desiredState
@@ -351,14 +404,15 @@ namespace Robocup.MotionControl
         {
             Constants.Load("motionplanning");
 
+            NUM_ROBOTS = Constants.get<int>("default", "NUM_ROBOTS");
+
             LOOK_AHEAD_DIST = Constants.get<double>("motionplanning", "LOOK_AHEAD_DIST");
             AVOID_DIST = Constants.get<double>("motionplanning", "AVOID_DIST");
             WAYPOINT_DIST = Constants.get<double>("motionplanning", "WAYPOINT_DIST");
             MIN_ABS_VAL_STICK = Constants.get<double>("motionplanning", "MIN_ABS_VAL_STICK");
-            EXTRA_GOAL_DIST = Constants.get<double>("motionplanning", "EXTRA_GOAL_DIST");
-        	STEADY_STATE_SPEED = Constants.get<double>("motionplanning", "STEADY_STATE_SPEED");
+            STEADY_STATE_SPEED = Constants.get<double>("motionplanning", "STEADY_STATE_SPEED");
 
-            
+
             MIN_X_ROBOT_BOUNDARY = Constants.get<double>("motionplanning", "MIN_X_ROBOT_BOUNDARY");
             MAX_X_ROBOT_BOUNDARY = Constants.get<double>("motionplanning", "MAX_X_ROBOT_BOUNDARY");
             MIN_Y_ROBOT_BOUNDARY = Constants.get<double>("motionplanning", "MIN_Y_ROBOT_BOUNDARY");
@@ -383,9 +437,6 @@ namespace Robocup.MotionControl
             boundary_lines.Add(new Line(bottomleft, bottomright));
             // right
             boundary_lines.Add(new Line(topright, bottomright));
-            
-            //ROTATE_ANGLE = Constants.get<double>("motionplanning", "ROTATE_ANGLE");
-            //ITER_INCREMENT = Constants.get<double>("motionplanning", "ITER_INCREMENT");
         }
 
         private bool isOutOfWay(Vector2 position, Vector2 start, Vector2 pathVector)
@@ -435,25 +486,6 @@ namespace Robocup.MotionControl
             return angleBetween;
         }
 
-        /// <summary>
-        /// Given a vector and a bool as to whether clockwise, rotate vector
-        /// </summary>
-        /// <param name="v"></param>
-        /// <param name="CW"></param>
-        /// <returns></returns>
-        private Vector2 rotate90(Vector2 v, bool CW)
-        {
-            if (CW)
-            {
-                return new Vector2(v.Y, -v.X);
-            }
-            return new Vector2(-v.Y, v.X);
-        }
-        private Vector2 rotateDegree(Vector2 v, double degree)
-        {
-            return new Vector2(v.X * Math.Cos(degree) - v.Y * Math.Sin(degree), v.X * Math.Sin(degree) + v.Y * Math.Cos(degree));
-        }
-
         private bool isGoodPath(List<Vector2> obstacles, Vector2 start, Vector2 pathVector)
         {
             foreach (Vector2 o in obstacles)
@@ -468,39 +500,39 @@ namespace Robocup.MotionControl
         }
     }
 
-	/// <summary>
-	/// A basic unidirectional RRT planner that searches over a Vector2 space. A wrapper around the generic BasicRRTPlanner.
-	/// The conversion Vector2->RobotInfo (to return RobotPath) is oversimplified - velocities are assigned with constant
-	/// speed in the direction of the next waypoint
-	/// </summary>
-	public class BasicRRTMotionPlanner : IPathPlanner
-	{
-		double AVOID_DIST;
-		double STEADY_STATE_SPEED;
+    /// <summary>
+    /// A basic unidirectional RRT planner that searches over a Vector2 space. A wrapper around the generic BasicRRTPlanner.
+    /// The conversion Vector2->RobotInfo (to return RobotPath) is oversimplified - velocities are assigned with constant
+    /// speed in the direction of the next waypoint
+    /// </summary>
+    public class BasicRRTMotionPlanner : IPathPlanner
+    {
+        double AVOID_DIST;
+        double STEADY_STATE_SPEED;
 
-		private readonly BasicRRTPlanner<Vector2, Vector2Tree> planner;
+        private readonly BasicRRTPlanner<Vector2, Vector2Tree> planner;
 
-		public BasicRRTMotionPlanner()
-		{
-			planner = new BasicRRTPlanner<Vector2, Vector2Tree>(Common.ExtendVV, Common.RandomStateV);
-		}
+        public BasicRRTMotionPlanner()
+        {
+            planner = new BasicRRTPlanner<Vector2, Vector2Tree>(Common.ExtendVV, Common.RandomStateV);
+        }
 
-		public RobotPath GetPath(Team team, int id, RobotInfo desiredState, IPredictor predictor, double avoidBallRadius)
-		{
-			List<Obstacle> obstacles = new List<Obstacle>();
-			//Aviod all robots, but myself
-			foreach (RobotInfo info in predictor.GetRobots())
-			{
-				if (info.Team != team || info.ID != id)
-					obstacles.Add(new Obstacle(info.Position, AVOID_DIST));
-			}
-			//If needed, avoid ball
+        public RobotPath GetPath(Team team, int id, RobotInfo desiredState, IPredictor predictor, double avoidBallRadius)
+        {
+            List<Obstacle> obstacles = new List<Obstacle>();
+            //Aviod all robots, but myself
+            foreach (RobotInfo info in predictor.GetRobots())
+            {
+                if (info.Team != team || info.ID != id)
+                    obstacles.Add(new Obstacle(info.Position, AVOID_DIST));
+            }
+            //If needed, avoid ball
             if (avoidBallRadius > 0 &&
                 predictor.GetBall() != null &&
                 predictor.GetBall().Position != null)
                 obstacles.Add(new Obstacle(predictor.GetBall().Position, avoidBallRadius));
 
-			RobotInfo curinfo;
+            RobotInfo curinfo;
             try
             {
                 curinfo = predictor.GetRobot(team, id);
@@ -509,29 +541,29 @@ namespace Robocup.MotionControl
             {
                 return new RobotPath(team, id);
             }
-			List<Vector2> path = planner.Plan(curinfo.Position, desiredState.Position, obstacles);
+            List<Vector2> path = planner.Plan(curinfo.Position, desiredState.Position, obstacles);
 
-			List<RobotInfo> robotPath = new List<RobotInfo>();
+            List<RobotInfo> robotPath = new List<RobotInfo>();
 
-			//Overly simplistic conversion from planning on position vectors to RobotInfo that has orientation and velocity information
-			//velocity at every waypoint just points to next one with constant speed
-			for (int i = 0; i < path.Count; i++)
-			{
-				//0-th waypoint is current state, driver won't like it
-				if (i == 0)
-					continue;
+            //Overly simplistic conversion from planning on position vectors to RobotInfo that has orientation and velocity information
+            //velocity at every waypoint just points to next one with constant speed
+            for (int i = 0; i < path.Count; i++)
+            {
+                //0-th waypoint is current state, driver won't like it
+                if (i == 0)
+                    continue;
 
-				RobotInfo waypoint = new RobotInfo(path[i], desiredState.Orientation, team, id);				
-				if (i < path.Count - 1)
-					waypoint.Velocity = (path[i + 1] - path[i]).normalizeToLength(STEADY_STATE_SPEED);
-				else
-					waypoint.Velocity = new Vector2(0, 0); //Stop at destination
-				
-				robotPath.Add(waypoint);
-			}
+                RobotInfo waypoint = new RobotInfo(path[i], desiredState.Orientation, team, id);
+                if (i < path.Count - 1)
+                    waypoint.Velocity = (path[i + 1] - path[i]).normalizeToLength(STEADY_STATE_SPEED);
+                else
+                    waypoint.Velocity = new Vector2(0, 0); //Stop at destination
 
-			return new RobotPath(robotPath);
-		}
+                robotPath.Add(waypoint);
+            }
+
+            return new RobotPath(robotPath);
+        }
 
 
         /*
@@ -587,11 +619,11 @@ namespace Robocup.MotionControl
 		}
          * */
 
-		public void ReloadConstants()
-		{
-			AVOID_DIST = Constants.get<double>("motionplanning", "AVOID_DIST");
-			STEADY_STATE_SPEED = Constants.get<double>("motionplanning", "STEADY_STATE_SPEED");
-		}
-	}
+        public void ReloadConstants()
+        {
+            AVOID_DIST = Constants.get<double>("motionplanning", "AVOID_DIST");
+            STEADY_STATE_SPEED = Constants.get<double>("motionplanning", "STEADY_STATE_SPEED");
+        }
+    }
 
 }
