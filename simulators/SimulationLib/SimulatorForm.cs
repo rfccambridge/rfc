f23e0afcb33eb6838c54e106d6c4f803a8363fd9
David Wu <davidwu@fas.harvard.edu>
2011-03-24 15:07:31 -0400

    Make simulator not crash when a socket exception occurs

--- simulators/SimulationLib/SimulatorForm.cs
+++ simulators/SimulationLib/SimulatorForm.cs
@@ -84,11 +84,20 @@ namespace Robocup.Simulation
 
                     _physicsEngine.SetScenario(lstScenarios.SelectedItem as SimulatedScenario);
 
-                    _physicsEngine.StartCommander(cmdPort);
-                    _physicsEngine.StartVision(visionIp, visionPort);
-                    
-                    if(chkReferee.Checked)
-                        _physicsEngine.StartReferee(refBoxIp, refBoxPort);
+                    try
+                    {
+                        _physicsEngine.StartCommander(cmdPort);
+                        _physicsEngine.StartVision(visionIp, visionPort);
+
+                        if (chkReferee.Checked)
+                            _physicsEngine.StartReferee(refBoxIp, refBoxPort);
+                    }
+                    catch (System.Net.Sockets.SocketException sock_exc)
+                    {
+                        MessageBox.Show(sock_exc.ToString());
+                        return;
+                    }
+
 
                     _physicsEngine.Start(numYellow, numBlue);
 
