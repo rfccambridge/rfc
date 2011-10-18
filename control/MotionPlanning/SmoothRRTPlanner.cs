e3aac86d2eb34cbe0c4f2c70e6fd80a7f48c8e00
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2011-10-18 01:39:48 -0400

    Remove Console.PrintLines.
    There were debug statements if desired state was in an obstacle.

--- control/MotionPlanning/SmoothRRTPlanner.cs
+++ control/MotionPlanning/SmoothRRTPlanner.cs
@@ -5,7 +5,7 @@ using System.Text;
 using Robocup.Core;
 using Robocup.CoreRobotics;
 using Robocup.Geometry;
-
+//using Robocup.CorePlayFiles;
 
 namespace Robocup.MotionControl
 {
@@ -776,12 +776,11 @@ namespace Robocup.MotionControl
             for (int i = 0; i < obstacles.Count; i++)
             {
                 Geom g = obstacles[i];
-                if (g is Rectangle && ((Rectangle)g).contains(desiredState.Position))
-                    Console.WriteLine("Warning: SmoothRRTPlanner desired state inside obstacle!");
-                else if (g is Circle && ((Circle)g).contains(desiredState.Position))
-                    Console.WriteLine("Warning: SmoothRRTPlanner desired state inside obstacle!");
+                //if (g is Rectangle && ((Rectangle)g).contains(desiredState.Position))
+                //    //Stats.Incr("RRT-state-inside-obstacle");
+                //else if (g is Circle && ((Circle)g).contains(desiredState.Position))
+                //    //Stats.Incr("RRT-state-inside-obstacle");
             }
-
 
             return GetPath(desiredState, predictor, avoidBallRadius, oldPath, obstacles);
         }
